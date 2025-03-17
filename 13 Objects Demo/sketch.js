2f971f81079e6807440beec4072ab5c487336439
JoyM0925 <joymin17306230925@gmail.com>
2025-03-17 12:00:52 -0600

    finished

--- 13 Objects Demo/sketch.js
+++ 13 Objects Demo/sketch.js
@@ -7,20 +7,33 @@ let bookshelf = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  //
-  myBook = new Book("CS30 Text", "Mr. Scott", 1234567891011, "leatherbound",515, width*0.3);
+  //Make 20 books in a row
+  let x = 50;
+  let covers = ["softcover","hardcover","leatherbound"];
+  for(let i = 0; i < 20; i++){
+    let choice = int(random(3));  //0, 1, 2
+  
+    bookshelf.push(new Book("A", "Mr. Booth", 1111111111, covers[choice], 200, x));
+    x += 20; 
+  }
+ 
+  myBook = new Book("CS30 Text", "Mr. Scott",
+    1234567891011, "leatherbound",
+    515, width * 0.3);
+
+
   myBook.printOut();
 }
 
 function draw() {
-  background(220);
+  background(220); //clears the screen each frame
   myBook.display();
 }
 
-// Mice to orgnize class at the bottom...
-class Book{
-  // 1. comstructor
-  constructor( title, author, isbn, cover, pages, x){
+// Nice to organize class at the bottom...
+class Book {
+  //1. constructor
+  constructor(title, author, isbn, cover, pages, x) {
     this.title = title;
     this.author = author;
     this.isbn = isbn;
@@ -29,29 +42,30 @@ class Book{
     this.x = x;
   }
 
-  // 2. Class Methods
-  display(){
+  //2. class methods
+  display() {
     //render our book object on the canvas
     rectMode(CENTER); textAlign(CENTER, CENTER);
     textSize(20);
 
-    switch(this.cover) {
+    switch (this.cover) {
     case "softcover":
-      fill(250,200,150);   break;
+      fill(250, 200, 150); break;
     case "hardcover":
-      fill(120,255,255);   break;
+      fill(120, 255, 255); break;
     case "leatherbound":
-      fill(150, 100, 15);  break;
-
+      fill(150, 100, 15); break;
     }
+
     rect(this.x, height/2, this.pages/10, 150);
     fill(255);
     text(this.title[0], this.x, height/2 - 50);
   }
+
   printOut(){
-    // print out the data in a nice format
-    print(this.title + ", by" + this.author);
-    print("Length:" + this.pages);
-    print("ISBN:" + this.isbn);
+    //print out the data in a nice format
+    print(this.title + ", by " + this.author);
+    print("Length: " + this.pages);
+    print("ISBN: " + this.isbn);
   }
 }
