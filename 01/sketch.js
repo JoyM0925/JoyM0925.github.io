cf2035640bda58fbb5afa7646a8422b865498772
JoyM0925 <joymin17306230925@gmail.com>
2025-09-10 14:09:37 -0600

    September 10

--- 01/sketch.js
+++ 01/sketch.js
@@ -1,13 +1,33 @@
+// Coordinate Systems and USer Events
 // Joy Min
-// Interactive program
+// September 10, 2025
+// A first look at ineractive progrms with p5.js
+
 
 function setup() { // runs Once at the first
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400,400);
+  print(windowWidth, windowHeight, width, height);
 }
 
 function draw() { // runs ove and over
                       // targeting 60 frames per second
-  // background(220); 
-  fill(255,mouseX,0);
+  background(220); // draws a big solid rectangle 
+  drawTwoCircles()
+}
+
+function drawTwoCircles(){ //[ALT][SHFIT][F] -> Autoformat
+  // draws two circles, onc at a fixed loation
+  // and one at the mouse location
+
+  // Draw order
+  // Later a thing is drawn
+  // The higher it is in the draw stack
+  
+  fill(255, 0, 255)
+  circle(width/2, height/2, 100);
+  fill(0, 255, 0);// colors are a bit like
+                  //picking up a marker, or
+                  //filling a paintbrush with paint...
+       //R,G,B
   circle(mouseX, mouseY, 30)
 }
