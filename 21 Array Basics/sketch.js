64e2bd4770e384607a49758afa743eef9eacef16
JoyM0925 <joymin17306230925@gmail.com>
2025-11-12 13:22:04 -0600

    fini

--- 21 Array Basics/sketch.js
+++ 21 Array Basics/sketch.js
@@ -1,5 +1,5 @@
 // 2D Array Basics
-// Mr. Scott
+// Joy Min
 // Nov 3, 2025
 
 
@@ -8,9 +8,9 @@
 
 let grid = [];
 
-let rows = 5; // y
-let cols = 5; // x
-let mode = 0;
+let rows = 10; // y
+let cols = 10; // x
+let mode = 0; // store a variable called mode, if it is odd number then square flip, even -> cross flip; it starts with 0 -> cross number
 let squareSize = 100;
 
 function setup() { 
@@ -21,7 +21,21 @@ function setup() {
 function draw() {
   background(220);
   renderGrid();
-  if (checkWiWHite() || checkWiBlack()) {
+
+  // overlays
+  if (keyIsDown(SHIFT)){
+    overlayOfFlip(getCurrentX(), getCurrentY()) // overlay of single title 
+  }
+  else{
+    if (mode%2 == 1){ // odd -> square 
+      overlayOfSquareflip(getCurrentX(), getCurrentY()) // overlay of square 
+    }
+    else{ // even -> cross 
+      overlayOfCrossflip(getCurrentX(), getCurrentY()) // overlay of cross 
+    }
+  }
+
+  if (checkWiWHite() || checkWiBlack()) { // check if the whole screen is black or white
     fill(51, 153, 255);
     textSize(32);
     textAlign(CENTER, CENTER);
@@ -42,7 +56,7 @@ function renderGrid(){
 }
 
 function keyPressed(){
-  if (key === " "){
+  if (key === " "){ // once the space bar pressed, the variable mode ++
     mode++;
   }
 }
@@ -53,15 +67,16 @@ function mousePressed(){
   let x = getCurrentX();
   let y = getCurrentY();
 
-  if (keyIsDown(SHIFT)){
+  // flip
+  if (keyIsDown(SHIFT)){ // single title flip
     flip(x,y)
   }
   else{
-    if (mode%2 == 1){
-      squareFlip(x,y)
+    if (mode%2 == 1){ //odd -> square flip
+      squareFlip(x,y) // overlay of square
     }
-    else{
-      crossflip(x,y);
+    else{ // even -> cross 
+      crossflip(x,y); // overlay of cross
     }
   }
 }
@@ -88,6 +103,19 @@ function flip(x, y){
   }
 }
 
+function overlayOfFlip(x,y){
+  if(x >= 0 && x < cols && y >= 0 && y < rows){ // check the boundary
+    if (grid[y][x] === 255){ // if the tile is white
+      fill(255, 204, 204); // cover with light pink
+      square(x * squareSize, y * squareSize, squareSize);
+    }
+    else { // if it is black
+      fill(255, 104,104); // cover with dark pink
+      square(x * squareSize, y * squareSize, squareSize);
+    }
+  }
+}
+
 function crossflip(x, y){
   //IF THEY EXIST:
   //flip our NSEW neighbours (cross pattern)
@@ -98,17 +126,113 @@ function crossflip(x, y){
   if(y+1 >= 0) flip(x, y+1);
 }
 
-function squareFlip(x,y){ 
-  flip(x, y)
-  if(x+1 < cols) flip(x+1,y);
-  if(y-1 >= 0) flip(x, y-1);
-  flip(x+1, y-1)
+function overlayOfCrossflip(x,y){
+  // center
+  if(x >= 0 && x < cols && y >= 0 && y < rows){
+    if (grid[y][x] === 255){
+      fill(255, 204, 204);
+    } else{
+      fill(255, 104,104);
+    }
+    square(x * squareSize, y * squareSize, squareSize);
+  }
+
+  // left
+  if (x-1 >= 0 && x-1 < cols && y >= 0 && y < rows){
+    if (grid[y][x-1] === 255){
+      fill(255, 204, 204);
+    } else{
+      fill(255, 104,104);
+    }
+    square((x-1) * squareSize, y * squareSize, squareSize);
+  }
+
+  // right
+  if (x+1 >= 0 && x+1 < cols && y >= 0 && y < rows){
+    if (grid[y][x+1] === 255){
+      fill(255, 204, 204);
+    } else{
+      fill(255, 104,104);
+    }
+    square((x+1) * squareSize, y * squareSize, squareSize);
+  }
+
+  // up
+  if (x >= 0 && x < cols && y-1 >= 0 && y-1 < rows){
+    if (grid[y-1][x] === 255){
+      fill(255, 204, 204);
+    } else{
+      fill(255, 104,104);
+    }
+    square(x * squareSize, (y-1) * squareSize, squareSize);
+  }
+
+  // down
+  if (x >= 0 && x < cols && y+1 >= 0 && y+1 < rows){
+    if (grid[y+1][x] === 255){
+      fill(255, 204, 204);
+    } else{
+      fill(255, 104,104);
+    }
+    square(x * squareSize, (y+1) * squareSize, squareSize);
+  }
 }
 
-function checkWiWHite() {
+function squareFlip(x,y){ 
+  flip(x, y) // left down
+  if(x+1 < cols) flip(x+1,y); // right down
+  if(y-1 >= 0) flip(x, y-1); // left up
+  flip(x+1, y-1) // right up
+}
+
+function overlayOfSquareflip(x,y){
+  // left dowwn
+  if(x >= 0 && x < cols && y >= 0 && y < rows){
+    if (grid[y][x] === 255){
+      fill(255, 204, 204);
+    } else{
+      fill(255, 104,104);
+    }
+    square(x * squareSize, y * squareSize, squareSize);
+  }
+
+  // right up
+  if(x+1 >= 0 && x+1 < cols && y-1 >= 0 && y-1 < rows){
+    if (grid[y-1][x+1] === 255){
+      fill(255, 204, 204);
+    } else{
+      fill(255, 104,104);
+    }
+    square((x+1) * squareSize, (y-1) * squareSize, squareSize);
+  }
+
+  // right down
+  if(x+1 >= 0 && x+1 < cols && y >= 0 && y < rows){
+    if (grid[y][x+1] === 255){
+      fill(255, 204, 204);
+    } else{
+      fill(255, 104,104);
+    }
+    square((x+1) * squareSize, y * squareSize, squareSize);
+  }
+
+  // left up
+  if(x >= 0 && x < cols && y-1 >= 0 && y-1 < rows){
+    if (grid[y-1][x] === 255){
+      fill(255, 204, 204);
+    } else{
+      fill(255, 104,104);
+    }
+    square(x * squareSize, (y-1) * squareSize, squareSize);
+  }
+}
+
+
+function checkWiWHite() { 
   let colorW = 255;
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
+      // check if each of the tile is white or not
       if (grid[y][x] !== colorW) {
         return false;
       }
@@ -121,6 +245,7 @@ function checkWiBlack() {
   let colorB = 0;
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
+      // check if each of the tile is black or not
       if (grid[y][x] !== colorB) {
         return false;
       }
@@ -131,13 +256,13 @@ function checkWiBlack() {
 
 function randomGrid(){
   for (let y = 0; y < rows; y++) {
-    grid[y] = [];
+    grid[y] = []; // push the # of rows of arrays into the grid[];
     for (let x = 0; x < cols; x++) {
-      if(random(1)<= 0.5){
-        grid[y][x] = 0;
+      if(random(1)<= 0.5){ // there is 50% of chance push the number(0 or 255)into the []
+        grid[y][x] = 0; // black
       }
       else {
-        grid[y][x] = 255;
+        grid[y][x] = 255; // white
       }
     }
   }
